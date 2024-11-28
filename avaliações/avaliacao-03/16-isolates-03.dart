8c8beadfc3b0091ea58599bc3b73e688d0736db5
MarinasQueiroz1 <148665025+MarinasQueiroz1@users.noreply.github.com>
2024-11-28 19:38:03 -0300

    Update 16-isolates-03.dart

--- avaliações/avaliacao-03/16-isolates-03.dart
+++ avaliações/avaliacao-03/16-isolates-03.dart
@@ -6,7 +6,7 @@ void main() async {
   // Criando um isolate para executar uma operação assíncrona
   final receivePort = ReceivePort();
   await Isolate.spawn(doAsyncOperation, receivePort.sendPort);
-
+  
   // Executando outras tarefas enquanto aguarda a conclusão da operação assíncrona
   print('Iniciando outras tarefas...');
   await Future.delayed(Duration(seconds: 1));
@@ -14,11 +14,11 @@ void main() async {
 
   // Recebendo o resultado da operação assíncrona
   final result = await receivePort.first;
-  print('Resultado: $result');
+  print('Resultado da Marina Silva: $result');
 }
 
 void doAsyncOperation(SendPort sendPort) async {
   // Executando uma operação assíncrona em um isolate separado
-  final result = await "Marina Silva";
+ final result = await File('./arquivo.txt').readAsString();
   sendPort.send(result);
 }
